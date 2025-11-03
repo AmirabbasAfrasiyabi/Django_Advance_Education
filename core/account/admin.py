1c7515080767dc090094bd2a050b28ffe3b7c330
Amirabbas Afrasiyabi <abb.afrasiyabi@gmail.com>
2025-11-03 21:15:54 +0330

    work with signal

--- core/account/admin.py
+++ core/account/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.forms import UserCreationForm , UserChangeForm
-from .models import User
+from .models import User,Profile
 from django import forms
 
 class CustomUserCreationForm(forms.ModelForm):
@@ -50,4 +50,5 @@ class CustomUserAdmin(UserAdmin):
             ),
         }),
     )
+admin.site.register(Profile)
 admin.site.register(User,CustomUserAdmin)
--- core/account/models.py
+++ core/account/models.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import (
     AbstractBaseUser,
     PermissionsMixin)
 from django.utils.translation import gettext_lazy as _
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
@@ -46,3 +48,22 @@ class User(AbstractBaseUser, PermissionsMixin):
     
     def __str__(self):
         return self.email
+
+class Profile(models.Model):
+    objects = None
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    image = models.ImageField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.email
+
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
