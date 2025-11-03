6827a71e9f32cdeef058c96553dce9c468d742f2
Amirabbas Afrasiyabi <abb.afrasiyabi@gmail.com>
2025-11-03 15:24:16 +0330

    UPDATED

--- core/account/admin.py
+++ core/account/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
-# Register your models here.
+
 
 class CustomUserAdmin(UserAdmin):
     model = User
+    # add_form = CustomUserCreationForm
     list_display = ("email" , "is_superuser" , "is_active")
     list_filter = ("email" , "is_superuser" , "is_active")
     search_fields = ("email",)
@@ -24,6 +25,17 @@ class CustomUserAdmin(UserAdmin):
             ),
         }),
 
+        ('group Permission', {
+            "fields": (
+                'groups', 'user_permissions'
+            ),
+        }),
+
+        ('important date', {
+            "fields": (
+                'last_login',
+            ),
+        }),
      )
     add_fieldsets = (
         ("Add New User", {
